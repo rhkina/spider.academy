0ee0343e477e356db191b9fa4053735b802a8283
rhkina <rhkina@gmail.com>
2024-03-31 22:03:05 -0300

    First commit

---
+++ src/lib/fonts.ts
@@ -0,0 +1,18 @@
+import { Audiowide, Slackey, DM_Sans } from 'next/font/google'
+
+export const dmsans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const audiowide = Audiowide({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
+
+export const slackey = Slackey({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+})
